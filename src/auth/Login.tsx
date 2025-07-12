4e82ab6686eb9f0af10a655ee3979e31f6ccf6d8
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-07-12 08:26:11 -0400

    fix: debug error with DescopeAuth

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,9 +1,25 @@
 import DescopeLogin from "./draftAuth/CombinedDescopeAuth";
+import DescopeAuth from "./descopeLogin/DescopeAuth";
 import SamplePage from "../components/SamplePage";
+import { AuthProvider } from "@descope/react-sdk";
 
 const Login = () => {
-  const loginChoice = "Descope";
-  return <>{loginChoice === "Descope" ? <DescopeLogin /> : SamplePage}</>;
+  const projectId = import.meta.env.VITE_DESCOPE_PROJECT_ID;
+
+  const loginChoice: string = "Descope";
+  return (
+    <>
+      {loginChoice === "OldDescope" ? (
+        <DescopeLogin />
+      ) : loginChoice === "Descope" ? (
+        <AuthProvider projectId={projectId}>
+          <DescopeAuth />
+        </AuthProvider>
+      ) : (
+        <SamplePage />
+      )}
+    </>
+  );
 };
 
 export default Login;
